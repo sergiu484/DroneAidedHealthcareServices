ed16e5e158d68918de611fcc356d4e6641047b36
sergiu484 <ghimpsergiu@gmail.com>
2020-06-22 22:04:17 +0300

    Commit 5

--- ParteaOP.py
+++ ParteaOP.py
@@ -663,13 +663,14 @@ def Euristic(I, R, Raza, k, Pk, costLow, costBig, Pi, Di):
 def Cost(a, b):
     return math.sqrt(pow(a[0][1] - b[0][1], 2) + pow(a[0][0] - b[0][0], 2))+a[1] - a[2] + b[1] - b[2]
 
-def Eliminare(path1,path2):
-    n1=copy.deepcopy(path1)
-    n2=copy.deepcopy(path2)
+
+def Eliminare(path1, path2):
+    n1 = copy.deepcopy(path1)
+    n2 = copy.deepcopy(path2)
     for i in path1:
         if i in path2:
-            aux1=copy.deepcopy(n1)
-            aux2=copy.deepcopy(n2)
+            aux1 = copy.deepcopy(n1)
+            aux2 = copy.deepcopy(n2)
             aux1.remove(i)
             aux2.remove(i)
             Puncte1 = [i[0] for i in n1]
@@ -682,43 +683,44 @@ def Eliminare(path1,path2):
                 s1 += i
             for i in Incarcatura2:
                 s2 += i
-            if DistantaCale(Puncte1)+s1<=DistantaCale(Puncte2)+s2:
-                n2= aux2
+            if DistantaCale(Puncte1)+s1 <= DistantaCale(Puncte2)+s2:
+                n2 = aux2
             else:
                 n1 = aux1
-    path1=n1
-    path2=n2
-    return path1,path2
+    path1 = n1
+    path2 = n2
+    return path1, path2
 
-def ImpartireInCai(tur,limita,centru,Pi,Di):
+
+def ImpartireInCai(tur, limita, centru, Pi, Di):
     Cai = [[]]
-    n=0
+    n = 0
     cale = []
     i = 0
-    while i !=len(tur):
-        cale2=copy.deepcopy(cale)
+    while i != len(tur):
+        cale2 = copy.deepcopy(cale)
         cale2.append(tur[i])
-        s=0
+        s = 0
         for j in cale2:
-            s+= Pi[j]-Di[j]
-        if ParteaSP.distanta2Puncte(centru, cale2[0]) + DistantaCale(cale2) + ParteaSP.distanta2Puncte(centru, cale2[-1]) <= limita and abs(s)<= limita:
+            s += Pi[j]-Di[j]
+        if ParteaSP.distanta2Puncte(centru, cale2[0]) + DistantaCale(cale2) + ParteaSP.distanta2Puncte(centru, cale2[-1]) <= limita and abs(s) <= limita:
 
             cale = cale2
             Cai[n] = cale
-            i+=1
+            i += 1
 
         else:
             if Cai[n] == []:
-                print('cale----',centru,cale2,ParteaSP.distanta2Puncte(centru, cale2[0]) + DistantaCale(cale2) + ParteaSP.distanta2Puncte(centru, cale2[-1]),cale)
+                print('cale----', centru, cale2, ParteaSP.distanta2Puncte(centru,
+                                                                          cale2[0]) + DistantaCale(cale2) + ParteaSP.distanta2Puncte(centru, cale2[-1]), cale)
                 break
             cale = []
             Cai.append([])
-            n+=1
+            n += 1
 
-        #print(Cai)
-        #print(tur)
+        # print(Cai)
+        # print(tur)
     return Cai
-
 
 
 def MiniAco(nodes):
@@ -729,9 +731,9 @@ def MiniAco(nodes):
     # or
     solutions = solver.solutions(world)
 
-    #print('distance',solution.distance)
-    #print('tour',solution.tour)    # Nodes visited in order
-    #print('path',solution.path)    # Edges taken in order
+    # print('distance',solution.distance)
+    # print('tour',solution.tour)    # Nodes visited in order
+    # print('path',solution.path)    # Edges taken in order
     # or
     best = float("inf")
     bestT = 0
@@ -739,12 +741,12 @@ def MiniAco(nodes):
         assert solution.distance < best
         best = solution.distance
         bestT = solution.tour
-    #bestT.insert(0,centru)
-    #bestT.append(centru)
+    # bestT.insert(0,centru)
+    # bestT.append(centru)
     return bestT
 
 
-def ACO(I, R, Raza, k, Pk, priceSmall, priceBig,Pi,Di):
+def ACO(I, R, Raza, k, Pk, priceSmall, priceBig, Pi, Di):
     Ri = ParteaSP.CentrePacient(I, R, Raza)
     R1, Ri = ParteaSP.SP(R, Ri)
     PuncteInCentru = dict()
@@ -758,50 +760,51 @@ def ACO(I, R, Raza, k, Pk, priceSmall, priceBig,Pi,Di):
     for i in R1:
         Pi[i] = 0
         Di[i] = 0
-    bestCai=[]
+    bestCai = []
     for i in PuncteInCentru:
-        for j in range(0,len(PuncteInCentru[i])):
-            PuncteInCentru[i][j]=(PuncteInCentru[i][j][0],Pi[PuncteInCentru[i][j][0]],Di[PuncteInCentru[i][j][0]])
-        print('Puncte',PuncteInCentru[i])
+        for j in range(0, len(PuncteInCentru[i])):
+            PuncteInCentru[i][j] = (
+                PuncteInCentru[i][j][0], Pi[PuncteInCentru[i][j][0]], Di[PuncteInCentru[i][j][0]])
+        print('Puncte', PuncteInCentru[i])
         bestCai.append(MiniAco(PuncteInCentru[i]))
     print(PuncteInCentru)
-    print('bestCai1',bestCai)
-    for i in range(0,len(bestCai)-1):
-        for j in range(i+1,len(bestCai)):
-            bestCai[i],bestCai[j]=Eliminare(bestCai[i],bestCai[j])
-    for i in range(0,len(bestCai)):
-        for j in range(0,len(bestCai[i])):
+    print('bestCai1', bestCai)
+    for i in range(0, len(bestCai)-1):
+        for j in range(i+1, len(bestCai)):
+            bestCai[i], bestCai[j] = Eliminare(bestCai[i], bestCai[j])
+    for i in range(0, len(bestCai)):
+        for j in range(0, len(bestCai[i])):
             bestCai[i][j] = bestCai[i][j][0]
-    print('Best',bestCai)
+    print('Best', bestCai)
 
     CaiFinale = []
-    for i in range(0,len(R1)):
+    for i in range(0, len(R1)):
         Lista = ImpartireInCai(bestCai[i], Pk[1], R1[i], Pi, Di)
-        for j in range(0,len(Lista)):
-            Lista[j].insert(0,R1[i])
+        for j in range(0, len(Lista)):
+            Lista[j].insert(0, R1[i])
             Lista[j].append(R1[i])
             CaiFinale.append(Lista[j])
-    print('CaiFinale',CaiFinale)
+    print('CaiFinale', CaiFinale)
     for i in CaiFinale:
         d = DistantaCale(i)
-        print('D=',d)
+        print('D=', d)
 
     ClasificareCai = []
     nrBig = 0
     nrSmall = 0
     for i in CaiFinale:
-        d=DistantaCale(i)
-        s=0
+        d = DistantaCale(i)
+        s = 0
         for j in i:
             s += Pi[j] - Di[j]
-        if d>Pk[0] or abs(s)>Pk[0]:
-            ClasificareCai.append([i,1])
-            nrBig+=1
+        if d > Pk[0] or abs(s) > Pk[0]:
+            ClasificareCai.append([i, 1])
+            nrBig += 1
         else:
             ClasificareCai.append([i, 0])
-            nrSmall+=1
+            nrSmall += 1
 
-    print('Clasif',ClasificareCai)
+    print('Clasif', ClasificareCai)
 
     xCentruDronaInainteEliminare = [i[0] for i in R]
     yCentruDronaInainteEliminare = [i[1] for i in R]
@@ -828,18 +831,21 @@ def ACO(I, R, Raza, k, Pk, priceSmall, priceBig,Pi,Di):
     # setarea Raza Centrului de Drona sa incapa complet in imagine
     xlimMin = min(xCentruDronaDupaEliminare) - (Raza + 1)
     xlimMax = max(xCentruDronaDupaEliminare) + (Raza + 1)
-    print("Maxim xCentruDrona = %d, Minim xCentruDrona = %d " % (xlimMax, xlimMin))
+    print("Maxim xCentruDrona = %d, Minim xCentruDrona = %d " %
+          (xlimMax, xlimMin))
 
     ylimMin = min(yCentruDronaDupaEliminare) - (Raza + 1)
     ylimMax = max(yCentruDronaDupaEliminare) + (Raza + 1)
-    print("Maxim yCentruDrona = %d, Minim yCentruDrona = %d " % (ylimMax, ylimMin))
+    print("Maxim yCentruDrona = %d, Minim yCentruDrona = %d " %
+          (ylimMax, ylimMin))
     # Construirea Dimensiunei Imaginei
     ax.set(xlim=(xlimMin, xlimMax), ylim=(ylimMin, ylimMax))
 
     # Pacientii
     xPacient = [i[0] for i in I]
     yPacient = [i[1] for i in I]
-    desenCoordonatePacienti = plt.scatter(xPacient, yPacient, c='b')  # pune pacientii pe axa
+    desenCoordonatePacienti = plt.scatter(
+        xPacient, yPacient, c='b')  # pune pacientii pe axa
     # numerotam pacientii
     n = 1
     for xy in zip(xPacient, yPacient):
@@ -850,7 +856,8 @@ def ACO(I, R, Raza, k, Pk, priceSmall, priceBig,Pi,Di):
     # Numerotam Centrele de Drona Inainte Eliminare
     n = 1
     for xy in zip(xCentruDronaInainteEliminare, yCentruDronaInainteEliminare):
-        numerotareCentreDrona = plt.annotate("D%d" % (n), xy=xy, textcoords='data')
+        numerotareCentreDrona = plt.annotate(
+            "D%d" % (n), xy=xy, textcoords='data')
         IdPuncte[xy] = "D%d" % (n)
         n += 1
     print("Id=", IdPuncte)
@@ -859,29 +866,29 @@ def ACO(I, R, Raza, k, Pk, priceSmall, priceBig,Pi,Di):
         j = i[0]
         c = i[1]
         if c == 0:
-    #deseneaza calea pentru drona de Tip 1
+            # deseneaza calea pentru drona de Tip 1
             nrSmall += 1
             for z in range(0, len(j)-1):
                 #plt.plot([j[z][0],j[z+1][0]],[j[z][1],j[z+1][1]],c = 'g', alpha = 0.6)
                 plt.annotate('', xy=(j[z+1][0], j[z+1][1]), xycoords='data',
                              xytext=(j[z][0], j[z][1]), textcoords='data',
-                             arrowprops=dict(frac=0.1, headwidth=5., width=0.5, color ="g"))
+                             arrowprops=dict(frac=0.1, headwidth=5., width=0.5, color="g"))
                 # print("j[z][0]=", j[z][0], "j[z][1]", j[z][1], "j[z+1][0]", j[z+1][0], "j[z+1][1]", j[z+1][1])
                 # print(j[z], j[z+1], j)
         elif c == 1:
-        # deseneaza calea pentru drona de tip2
+            # deseneaza calea pentru drona de tip2
             nrBig += 1
-            for z in range(0,len(j)-1):
+            for z in range(0, len(j)-1):
                 #plt.plot([j[z][0],j[z+1][0]],[j[z][1],j[z+1][1]],c = 'y', alpha = 0.6)
                 plt.annotate('', xy=(j[z + 1][0], j[z + 1][1]), xycoords='data',
                              xytext=(j[z][0], j[z][1]), textcoords='data',
                              arrowprops=dict(frac=0.1, headwidth=5., width=0.5, color="y"))
-    #Salveaza desenul
+    # Salveaza desenul
     plt.savefig(os.path.join('.', 'ant1.png'), bbox_inches='tight')
     plt.close()
 
     fig, ax = plt.subplots()
-    print('R1=',R1)
+    print('R1=', R1)
     for i in R1:
         a_circle = plt.Circle(i, Raza, color='r', linewidth=0.5, fill=False)
         ax.add_artist(a_circle)
@@ -897,31 +904,32 @@ def ACO(I, R, Raza, k, Pk, priceSmall, priceBig,Pi,Di):
     n = 1
     for xy in zip(xCentruDronaInainteEliminare, yCentruDronaInainteEliminare):
         if xy in R1:
-            numerotareCentreDrona = plt.annotate("D%d" % (n), xy=xy, textcoords='data')
+            numerotareCentreDrona = plt.annotate(
+                "D%d" % (n), xy=xy, textcoords='data')
         n += 1
-    #==
+    # ==
     for i in ClasificareCai:
         j = i[0]
         c = i[1]
         if c == 0:
-    #deseneaza calea pentru drona de Tip 1
+            # deseneaza calea pentru drona de Tip 1
             for z in range(0, len(j)-1):
                 #plt.plot([j[z][0],j[z+1][0]],[j[z][1],j[z+1][1]],c = 'g', alpha = 0.6)
                 plt.annotate('', xy=(j[z+1][0], j[z+1][1]), xycoords='data',
                              xytext=(j[z][0], j[z][1]), textcoords='data',
-                             arrowprops=dict(frac=0.1, headwidth=5., width=0.5, color ="g"))
+                             arrowprops=dict(frac=0.1, headwidth=5., width=0.5, color="g"))
                 # print("j[z][0]=", j[z][0], "j[z][1]", j[z][1], "j[z+1][0]", j[z+1][0], "j[z+1][1]", j[z+1][1])
                 # print(j[z], j[z+1], j)
         elif c == 1:
-        # deseneaza calea pentru drona de tip2
-            for z in range(0,len(j)-1):
+            # deseneaza calea pentru drona de tip2
+            for z in range(0, len(j)-1):
                 #plt.plot([j[z][0],j[z+1][0]],[j[z][1],j[z+1][1]],c = 'y', alpha = 0.6)
                 plt.annotate('', xy=(j[z + 1][0], j[z + 1][1]), xycoords='data',
                              xytext=(j[z][0], j[z][1]), textcoords='data',
                              arrowprops=dict(frac=0.1, headwidth=5., width=0.5, color="y"))
 
     plt.savefig(os.path.join('.', 'ant2.png'), bbox_inches='tight')
-    #plt.show()
+    # plt.show()
     plt.close()
     DateTabel = dict()
     PretCentru = dict()
@@ -966,12 +974,13 @@ def ACO(I, R, Raza, k, Pk, priceSmall, priceBig,Pi,Di):
     print('Clasif ', ClasificareCai)
     return DateTabel, PretCentru, TipDroneCentru, DistantaTotala, NrDroneMici, NrDroneMari, PretTotal, IdPuncte, len(I)
 
+
 def Tabel(grid, DateTabel, PretCentru, TipDroneCentru, DistantaTotala, NrDroneMici, NrDroneMari, PretTotal, IdPuncte, nrPacienti, timp, Pi, Di):
 
-    p=0
+    p = 0
     for i in IdPuncte:
         if IdPuncte[i].find('D') != 0:
-            p+=1
+            p += 1
     grid.CreateGrid(len(PretCentru)+4+p, 7)
 
     # We can set the sizes of individual rows and columns
@@ -1026,17 +1035,14 @@ def Tabel(grid, DateTabel, PretCentru, TipDroneCentru, DistantaTotala, NrDroneMi
                 n, 4) + str(j[1]) + ' | ')
         n += 1
 
-    grid.SetCellValue(len(PretCentru) + 3, 0,'Pacienti cu Cantitatea de ridicare si primire')
+    grid.SetCellValue(len(PretCentru) + 3, 0, 'Pacienti')
     grid.SetCellValue(len(PretCentru) + 3, 1, 'Ridicare')
     grid.SetCellValue(len(PretCentru) + 3, 2, 'Livrare')
 
-
-    m=0
+    m = 0
     for i in IdPuncte:
         if IdPuncte[i].find('D') != 0:
             grid.SetCellValue(len(PretCentru) + 4+m, 0, str(IdPuncte[i]))
             grid.SetCellValue(len(PretCentru) + 4 + m, 1, str(Pi[i]))
             grid.SetCellValue(len(PretCentru) + 4 + m, 2, str(Di[i]))
-            m+=1
-
-
+            m += 1
